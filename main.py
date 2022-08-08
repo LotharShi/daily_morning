f46c18d032dc2fec9605c99cb96a4629a7b00df0
LotharShi <85300746+LotharShi@users.noreply.github.com>
2022-08-08 09:54:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from datetime import date, datetime
+from datetime import datetime, timedelta
 import math
 from wechatpy import WeChatClient
 from wechatpy.client.api import WeChatMessage, WeChatTemplate
@@ -6,7 +6,7 @@ import requests
 import os
 import random
 
-today = datetime.now()
+today = datetime.now() + timedelta(hours=8)
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
@@ -16,6 +16,7 @@ app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
 
 user_id = os.environ["USER_ID"]
+my_id = os.environ["MY_ID"]
 template_id = os.environ["TEMPLATE_ID"]
 
 weekday_dict = {1:u"一",2:u"二",3:u"三",4:u"四",5:u"五",6:u"六",7:u"天"}
@@ -31,7 +32,7 @@ def get_count():
   return delta.days
 
 def get_left(theday):
-  next = datetime.strptime(str(date.today().year) + "-" + theday, "%Y-%m-%d")
+  next = datetime.strptime(str(today.year) + "-" + theday, "%Y-%m-%d")
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
   return (next - today).days
@@ -54,10 +55,10 @@ def air_color(q):
         return '#B22222'
 
 def get_today():
-    year = str(date.today().year)
-    month = str(date.today().month)
-    day = str(date.today().day)
-    weekday = weekday_dict[date.today().weekday()+1]
+    year = str(today.year)
+    month = str(today.month)
+    day = str(today.day)
+    weekday = weekday_dict[today.weekday()+1]
     
     return year+'年'+month+'月'+day+'日'+' 星期'+weekday
 
@@ -67,5 +68,8 @@ client = WeChatClient(app_id, app_secret)
 wm = WeChatMessage(client)
 weather, temperature, airData, airQuality = get_weather()
 data = {"today":{"value":get_today(), "color":"#FFC0CB"}, "weather":{"value":weather, "color":"#808080"}, "temperature":{"value":temperature, "color":"#002FA7"}, "air":{"value":airData+' '+airQuality, "color":air_color(airQuality)}, "love_days":{"value":get_count(), "color": '#D70000'}, "birthday":{"value":get_left(birthday), "color":'#FF6347'}, "anniversary_left":{"value":get_left(anniversary), "color":'#FF69B4'}, "words":{"value":get_words(), "color":get_random_color()}}
-res = wm.send_template(user_id, template_id, data)
-print(res)
+res_user = wm.send_template(user_id, template_id, data)
+res_my = wm.send_template(my_id, template_id, data)
+print(res_user)
+print(res_my)
+print(today)
